b2e2de121017b055bfde8ffa4b11082bb51ca3a5
Parcly Taxel <reddeloostw@gmail.com>
2021-09-18 22:12:42 +0800

    Dean Hickerson's drifter loops

--- skopje/justfriends.py
+++ skopje/justfriends.py
@@ -112,4 +112,76 @@ def phase_shifting_loop(p):
         pat = pat[26*x]
     return (pat, None)
 
-cfuncs = (p26_shuttle, p26_loop, phase_shifting_loop)
+dl1 = lt.pattern("""20bo2bo$13b6obo2bo$11b2o9bo$18b3obob3o3bo$10b3obo7bo4b2obo2bo$7bo6bobo
+b3o2bo6bo2bo$7bob4obobo5bo3b2o2bobo2b2o$7bo10bobobo7bobobo$7bo5b2o3bob
+obo2b2o3bobobo$9bo8bo3bo7bobobo$9bobob3o2bo4b2obobo5bo$6bo4bo6bob2o4bo
+bob3o2bo$bo4bo6bobobo3bo2bobo7bo$bob2o3bo4bobo2bo5bo5b3obo$bo3b3ob4o2b
+o2bo5bo9bo$bo12bo10b6obo2bo$o2b2obobob3obo7b2o8bo2bo$obo3bobo14bo2bobo
+b2o$2bobobobob7o9bobo4b4o$4bo21bo4b2o$b2obo3b2o5bo$4bo8bobo$3obo2b3obo
+bo2bo$4bo6bo4bo$5b3o8bo$2b2o$3bo""")(-34,-11)
+dl2 = lt.pattern("""38bo$38bo4bo3b2o$39b4ob3o$36b2o$37bo2b2obobob2o$20bo2bo19bobo$13b6obo
+2bo10b2ob5obobobo$11b2o9bo13bo10bo$18b3obob3o3bo11b2o3bo$10b3obo7bo4b
+2obo3b3obo8bo$7bo6bobob3o2bo6bob2o4bobob3o2bo$7bob4obobo5bo3b2o2bo4b2o
+bobo6bo$7bo10bobobo7bobobo7bobobo$7bo5b2o3bobobo2b2o3bobobo2b2o3bobobo
+$9bo8bo3bo7bobobo7bobo$9bobob3o2bo4b2obobo5bo3b2o2bo$6bo4bo6bob2o4bobo
+b3o2bo$bo4bo6bobobo3bo2bobo7bo5b4o$bob2o3bo4bobo2bo5bo5b3obob2o$bo3b3o
+b4o2bo2bo5bo9bo$bo12bo10b6obo2bo$o2b2obobob3obo7b2o8bo2bo$obo3bobo14bo
+2bobob2o$2bobobobob7o9bobo4b4o$4bo21bo4b2o$b2obo3b2o5bo$4bo8bobo$3obo
+2b3obobo2bo$4bo6bo4bo$5b3o8bo$2b2o$3bo""")(-43,-2)
+dl3 = lt.pattern("""6bo11bo$6bo4bo3b2obo$7b4ob3o3bo$4b2o12bo$6b4obobob2o2bo$11bobo3bobo$3b
+7obobobobo$15bo$4bo5b2o3bob2o$4bobo8bo$3bo2bobob3o2bob3o$3bo4bo6bo$3bo
+6bob3o$2o3bo4bo5b2o$2b3ob4o6bo$11b2o$2obobob2o2bo$3bobo$bobobob5ob2o$b
+o10bo$bo3b2o11bo3b3o$bo8bob3o3bob2o$bo2b3obobo4b2obo$bo6bobob2o4bo2b2o
+$2bobobo7bobobo$2bobobo3b2o2bobobo3b2o$4bobo7bobobo$6bo2b2o3bo5bobo$
+13bo2b3obobo$5b4o5bo7bobo$11b2obob3o5bo$14bo9bo$13bo2bob6o$13bo2bo8b2o
+$17b2obobo2bo$12b4o4bobo$16b2o4bo""")(-11,-2)
+dl4 = lt.pattern("""4b2o4bo$4o4bobo$5b2obobo2bo$bo2bo8b2o$bo2bob6o$2bo9bo$2bob3o5bo5bo3b3o
+$2bo7bobo2bo2bob2o$bo2b3obobo4bo2bo$2bo5bobob2o2bobo2b2o$2bobobo7bobob
+o$2bobobo3b2o2bobobo3b2o$2bobobo7bobobo$2o2bobo2b2o3bo5bobo$3bo2bo6bo
+2b3obobo$3bo2bob2o4bo7bobo$6bo3b3obob3o5bo$14bo9bo$13bo2bob6o$13bo2bo
+8b2o$17b2obobo2bo$12b4o4bobo$16b2o4bo""")(-2,-8)
+
+def drifter_loop(p):
+    """Construct a drifter loop of period p using Dean Hickerson's
+    universal 23-gen and 27-gen components. Requires the PuLP solver
+    for finding the optimal loop parameters through integer linear programming."""
+    if p < 13:
+        return None
+    # Minimise 71x + 36y subject to x, y >= 1 and q | 50x + 23y
+    # where q is a quarter of the "effective period" (p times how many drifters
+    # are needed to get a multiple of 4). The actual x and y are then these
+    # variables decremented
+    from pulp import LpProblem, LpVariable, PULP_CBC_CMD
+    q = p if p%2 else p//2 if p%4 else p//4
+    lp = LpProblem()
+    a = LpVariable("a", cat="Integer")
+    b = LpVariable("b", cat="Integer")
+    lp += (6*q*b - 23*a >= 1, "x")
+    lp += (50*a - 13*q*b >= 1, "y")
+    lp += (71*(6*q*b - 23*a) + 36*(50*a - 13*q*b), "population")
+    lp.solve(PULP_CBC_CMD(msg=False))
+    sols = {v.name: int(v.varValue) for v in lp.variables()}
+    a, b = sols["a"], sols["b"]
+    x = 6*q*b - 23*a - 1
+    y = 50*a - 13*q*b - 1
+    looplen = 200*(x+1) + 92*(y+1)
+
+    pat = dl1(0,0)
+    cx, cy = -20, 15
+    for _ in range(x):
+        pat += dl2(cx,cy)
+        cx -= 29
+        cy += 29
+    pat += dl3(cx, cy)
+    cx += 15
+    cy += 20
+    for _ in range(y):
+        pat += dl4(cx, cy)
+        cx += 24
+    pat += pat("rot180", cx, cy)
+    for _ in range(looplen // p):
+        pat[0,0] = 1
+        pat = pat[p]
+    return (pat, None)
+
+cfuncs = (p26_shuttle, p26_loop, phase_shifting_loop, drifter_loop)
